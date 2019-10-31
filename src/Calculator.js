f9985ef812d6fa9c25afa774e5f867949a758de1
Muhamad Risman <real.risman@gmail.com>
2019-10-31 21:48:51 +0700

    Adding a second input

---
+++ src/Calculator.js
@@ -0,0 +1,15 @@
+import React from "react";
+import TemperatureInput from "./TemperatureInput";
+
+class Calculator extends React.Component {
+  render() {
+    return (
+      <div>
+        <TemperatureInput scale="c" />
+        <TemperatureInput scale="f" />
+      </div>
+    );
+  }
+}
+
+export default Calculator;
---
+++ src/TemperatureInput.js
@@ -0,0 +1,34 @@
+import React from "react";
+import BoilingVerdict from "./BoilingVerdict";
+
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit"
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = { temperature: "" };
+  }
+
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
+  }
+
+  render() {
+    const temperature = this.state.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+
+        <BoilingVerdict celsius={parseFloat(temperature)} />
+      </fieldset>
+    );
+  }
+}
+
+export default TemperatureInput;

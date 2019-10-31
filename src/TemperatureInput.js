21289caf816b71ff672f3d524eaa9cf13a16abed
Muhamad Risman <real.risman@gmail.com>
2019-10-31 22:06:27 +0700

    Lifting state up

--- src/TemperatureInput.js
+++ src/TemperatureInput.js
@@ -1,5 +1,4 @@
 import React from "react";
-import BoilingVerdict from "./BoilingVerdict";
 
 const scaleNames = {
   c: "Celsius",
@@ -10,22 +9,19 @@ class TemperatureInput extends React.Component {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
-    this.state = { temperature: "" };
   }
 
   handleChange(e) {
-    this.setState({ temperature: e.target.value });
+    this.props.onTemperatureChange(e.target.value);
   }
 
   render() {
-    const temperature = this.state.temperature;
+    const temperature = this.props.temperature;
     const scale = this.props.scale;
     return (
       <fieldset>
         <legend>Enter temperature in {scaleNames[scale]}:</legend>
         <input value={temperature} onChange={this.handleChange} />
-
-        <BoilingVerdict celsius={parseFloat(temperature)} />
       </fieldset>
     );
   }
